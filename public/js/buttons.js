1daf04438d88417ea2c5b416c1766f75387a82f5
Jose Pedro Valentim Oliveira <zeca1997@hotmail.com>
2020-06-10 22:20:03 +0200

    hmm

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -5,14 +5,18 @@ fetch("../json/games.json").then((resp) => {
     return resp.json();
 }).then((data) => {
     data.forEach(game => {
+        
         let button = document.createElement('button');
         let img = document.createElement('img');
 
         img.src = `./images/${game.short}_Logo.png`;
         let link = game.name.split(" ").join("");
         button.onclick = ()=>{ window.location.href = `/signup/${link}`;};
-
-
+        if(game.short == "LoR" || game.short == "Val"){
+            button.disabled = true;
+            img.style.opacity = "0.5";
+        }
+        
         button.appendChild(img);
         buttonDiv.appendChild(button);
     });
