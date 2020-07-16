fd77a7b405cc8fa34cfec35df9d8ab8380366378
unknown <OPANT002670@outpost.local>
2020-07-16 14:06:53 +0200

    Links

--- routes/signupLinks.js
+++ routes/signupLinks.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/Event')
+const { ensureAuthenticated } = require('../config/auth');
 
 Event.find({}).then((events)=>{
     events.forEach(event=>{
+   
         router.get(`/${event.eventCode}`,(req,res)=>{
+            res.render('eventPage',{event:event})
+        })
+        router.get(`/signup/${event.eventCode}`,ensureAuthenticated,(req,res)=>{
             res.render('eventPage',{event:event})
         })
     })
